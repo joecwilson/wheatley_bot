1b28ad86b10881bd8a424c01908f295ab97bace6
joseph <josephcwilson47@gmail.com>
2023-05-05 15:20:53 -0500

    Initial implem,entatun of alpha beta pruning

--- src/engine.rs
+++ src/engine.rs
@@ -1,9 +1,10 @@
 use cozy_chess::Square::A1;
 use cozy_chess::{Board, Color, GameStatus, Move, Piece};
+use std::cmp::max;
 
-/// Returns a legal move that places the color in the worst position
+/// Returns a legal move that places the player to move in the worst position
 pub fn get_move(board: &Board) -> Move {
-    let mut min_eval: f32 = f32::NEG_INFINITY;
+    let mut max_eval: f32 = f32::INFINITY;
     let mut cur_move = Move {
         from: A1,
         to: A1,
@@ -11,57 +12,77 @@ pub fn get_move(board: &Board) -> Move {
     };
     board.generate_moves(|moves| {
         for mv in moves {
-            let cur_eval = get_evaluation(&mv, &board, 3);
-            if cur_eval >= min_eval {
+            let cur_eval = get_evaluation(&mv, &board, 3, f32::NEG_INFINITY, f32::INFINITY);
+            if cur_eval <= max_eval {
                 cur_move = mv;
-                min_eval = cur_eval;
+                max_eval = cur_eval;
             }
+            println!("The current evaluation is {cur_eval}");
         }
         false
     });
     cur_move
 }
 
-/// Returns the evaluation for a specific move.
-/// Higher evaluation == Good for the side to move
-/// We want to pick the lowest evaluation
-/// INFINITY = Size to move Wins
-/// -INFINITY = Other side Wins
+/// Returns the evaluation for a specific move. Assumes players will pick the move that hurts them the most
 /// depth = the amount of ply to search down. 0 is base case, 1 makes opponent move and stops
-fn get_evaluation(piece_move: &Move, board: &Board, depth: i32) -> f32 {
-    // Get how many pieces exist
+/// alpha = minimum score that the maximizing player is assured of
+/// beta = maximum score that the minimizing player is assured of.
+fn get_evaluation(piece_move: &Move, board: &Board, depth: i32, alpha: f32, beta: f32) -> f32 {
     let mut board_with_move = board.clone();
     board_with_move.play_unchecked(piece_move.clone());
-    if depth <= 0 {
-        match board_with_move.status() {
-            GameStatus::Drawn => return 0.0,
-            GameStatus::Won => return f32::NEG_INFINITY,
-            GameStatus::Ongoing => (),
-        }
-        // TODO: Implement 3 move repition
-
+    match board_with_move.status() {
+        GameStatus::Drawn => return 0.0,
+        GameStatus::Won => return f32::NEG_INFINITY,
+        GameStatus::Ongoing => (),
+    }
+    if depth == 0 {
         let mut evaluation: f32 = 0.0;
         evaluation += get_material_evaluation(&board_with_move) as f32;
         return evaluation;
-    } else {
-        // Assume other side helps us as much as possible. For them THEY are side to move
-        let mut eval = f32::NEG_INFINITY;
+    }
+    let mut evaluation;
+    let mut new_alpha = alpha;
+    let mut new_beta = beta;
+    if board.side_to_move() == Color::Black {
+        evaluation = f32::NEG_INFINITY;
         board.generate_moves(|moves| {
             for mv in moves {
-                let cur_eval = get_evaluation(&mv, &board, depth - 1);
-                if cur_eval >= eval {
-                    //cur_move = mv;
-                    eval = cur_eval;
+                let potential_eval = get_evaluation(&mv, &board, depth - 1, new_alpha, new_beta);
+                if potential_eval > evaluation {
+                    evaluation = potential_eval;
+                }
+                if evaluation > new_beta {
+                    return true;
+                }
+                if new_alpha > evaluation {
+                    new_alpha = evaluation;
                 }
             }
             false
         });
-        return eval;
+    } else {
+        evaluation = f32::INFINITY;
+        board.generate_moves(|moves| {
+            for mv in moves {
+                let potential_eval = get_evaluation(&mv, &board, depth - 1, new_alpha, new_beta);
+                if potential_eval < evaluation {
+                    evaluation = potential_eval;
+                }
+                if evaluation < new_alpha {
+                    return true;
+                }
+                if evaluation < new_beta {
+                    new_beta = evaluation;
+                }
+            }
+            false
+        });
     }
+    return evaluation;
 }
 
-/// Returns the material evaluation of a particular board
-/// Simply counts up the material values
+/// Returns the material evaluation of a particular board for white
 fn get_material_evaluation(board: &Board) -> i32 {
     let mut material: i32 = 0;
     material += board.colored_pieces(Color::White, Piece::Pawn).len() as i32;
@@ -75,10 +96,5 @@ fn get_material_evaluation(board: &Board) -> i32 {
     material -= board.colored_pieces(Color::Black, Piece::Bishop).len() as i32 * 3;
     material -= board.colored_pieces(Color::Black, Piece::Rook).len() as i32 * 5;
     material -= board.colored_pieces(Color::Black, Piece::Queen).len() as i32 * 9;
-    // if black is current move, we need to flip this
-
-    if board.side_to_move() == Color::Black {
-        material *= -1;
-    }
     return material;
 }

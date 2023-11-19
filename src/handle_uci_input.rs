a0bcae7b40020fa75ea0948407cf7e318337e130
joseph <josephcwilson47@gmail.com>
2023-11-19 16:25:27 -0600

    Added 3 fold reptiton check

--- src/handle_uci_input.rs
+++ src/handle_uci_input.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::thread::{self};
 
 use cozy_chess::{Board, Move};
@@ -11,7 +12,7 @@ pub fn debug(game: Game) -> Game {
         debug_mode: true,
         forced_capture: game.forced_capture,
         board: game.board,
-        move_history: game.move_history,
+        previous_boards: game.previous_boards,
     }
 }
 
@@ -25,7 +26,7 @@ pub fn set_option(_input_tokens: &Vec<&str>, game: Game) -> Game {
 }
 
 pub fn position(input_tokens: &Vec<&str>, game: Game) -> Game {
-    let mut move_history: Vec<Move> = Vec::new();
+    let mut previous_boards: HashMap<u64, i32> = HashMap::new();
     // First token is the position command
     if input_tokens[1] != "startpos" {
         handle_uci_output::send_info("Requires a starting point of startpos");
@@ -36,26 +37,29 @@ pub fn position(input_tokens: &Vec<&str>, game: Game) -> Game {
     //     panic!("Invalid UCI Command")
     // }
     let mut board = Board::startpos();
+    previous_boards.insert(board.hash(), 1);
     for i in 3..input_tokens.len() {
         let played_move = input_tokens[i].parse::<Move>().unwrap();
-        move_history.push(played_move);
+        // move_history.push(played_move);
         board.play(played_move);
+        let board_hash = board.hash();
+        previous_boards.entry(board_hash).and_modify(|board_hash| *board_hash += 1).or_insert(1);
     }
     Game {
-        debug_mode: true,
+        debug_mode: game.debug_mode,
         forced_capture: game.forced_capture,
         board: board,
-        move_history: move_history,
+        previous_boards: previous_boards,
     }
 }
 
 pub fn go(_input_tokens: &Vec<&str>, game: Game) -> Game {
     let new_game = game.clone();
     let _thread = thread::spawn(move || {
-        let engine_response = get_move(&game.board);
+        let engine_response = get_move(new_game);
         handle_uci_output::best_move(&engine_response.0.unwrap())
     });
-    new_game
+    game
 }
 
 pub fn stop(game: Game) -> Game {
@@ -79,6 +83,6 @@ pub fn default_game() -> Game {
         debug_mode: false,
         board: Board::startpos(),
         forced_capture: false,
-        move_history: Vec::new(),
+        previous_boards: HashMap::new(),
     }
 }
--- src/play.rs
+++ src/play.rs
@@ -1,9 +1,20 @@
-use cozy_chess::{Board, Move};
+use std::collections::HashMap;
+
+use cozy_chess::Board;
 
 #[derive(Clone)]
 pub struct Game {
+    /// Weigher to print more output when printing a move
     pub debug_mode: bool,
+    /// If captures should be able to be forced
     pub forced_capture: bool,
+    /// The current state of the board in the game
     pub board: Board,
-    pub move_history: Vec<Move>,
+    /// A field to aid in collecting 3 and 5 move repetition
+    /// The keys of this hashmap are the hashes of the board
+    /// This is not competly safe from collisions, but is likely
+    /// good enough for our needs
+    /// The values show the number of times that this 
+    /// move has been collected
+    pub previous_boards: HashMap<u64, i32>, 
 }
